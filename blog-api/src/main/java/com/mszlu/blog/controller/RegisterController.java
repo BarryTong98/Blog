c47bc057fd11cf67947f7228adc1b3af33ebdd0c
barrytong <371202041@qq.com>
2021-09-03 18:35:26 +0800

    start the project - blog

---
+++ blog-api/src/main/java/com/mszlu/blog/controller/RegisterController.java
@@ -0,0 +1,23 @@
+package com.mszlu.blog.controller;
+
+import com.mszlu.blog.service.LoginService;
+import com.mszlu.blog.vo.Result;
+import com.mszlu.blog.vo.params.LoginParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("register")
+public class RegisterController {
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping
+    public Result register(@RequestBody LoginParam loginParam){
+        //sso 单点登陆 后期如果把登陆注册功能 提出去（单独的服务，可以独立提供接口服务）
+        return loginService.register(loginParam);
+    }
+}
