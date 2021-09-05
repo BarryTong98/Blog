4bd1a5519dce8530071bc31f9552c50d89bed09a
barrytong <371202041@qq.com>
2021-09-05 11:55:07 +0800

    image upload functionality with qiniuyun cloud

--- blog-api/src/main/java/com/mszlu/blog/controller/ArticleController.java
+++ blog-api/src/main/java/com/mszlu/blog/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.mszlu.blog.controller;
 
+import com.mszlu.blog.common.aop.LogAnnotation;
 import com.mszlu.blog.service.ArticleService;
 import com.mszlu.blog.vo.ArticleVo;
 import com.mszlu.blog.vo.Result;
@@ -15,14 +16,16 @@ public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
-    //Result是统一结果返回
 
+    //Result是统一结果返回
     /**
      * 首页 文章 列表
      * @param pageParams
      * @return
      */
     @PostMapping
+    //加上此注解 代表要对此接口记录日志
+    @LogAnnotation(module="文章", operator = "获取文章列表")
     public Result listArticle(@RequestBody PageParams pageParams) {
         //ArticleVo 页面接收的数据
         return articleService.listArticle(pageParams);
@@ -62,7 +65,6 @@ public class ArticleController {
     @PostMapping("publish")
     public Result publish(@RequestBody ArticleParam articleParam){
         return  articleService.publish(articleParam);
-
     }
 }
 
---
+++ blog-api/src/main/java/com/mszlu/blog/controller/UpdateController.java
@@ -0,0 +1,38 @@
+package com.mszlu.blog.controller;
+
+import com.mszlu.blog.utils.QiniuUtils;
+import com.mszlu.blog.vo.Result;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("upload")
+public class UpdateController {
+    @Autowired
+    private QiniuUtils qiniuUtils;
+
+    @PostMapping
+    public Result upload(@RequestParam("image")MultipartFile file ){
+        //原始文件名称 比如aa.png
+        String originalFilename = file.getOriginalFilename();
+        //StringUtils.substringAfterLast(originalFilename, ".");
+        //如果originalFilename 是aa.png 那么我们就会获得点后面的数据 -》 png
+        //唯一的文件名称
+        String fileName = UUID.randomUUID().toString() + "." + StringUtils.substringAfterLast(originalFilename, ".");
+        //上传文件 上传到哪里呢？
+        //七牛云 云服务器 按量付费 速度快 把图片放到离用户最近的服务器上
+        //降低 我们自身应用服务器的带宽消耗
+        boolean upload = qiniuUtils.upload(file, fileName);
+        if(upload){
+            return Result.success(QiniuUtils.url + fileName);
+        }
+        return Result.fail(20001,"上传失败");
+    }
+}
